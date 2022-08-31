5aa14f8ed17be656739f3b9e278e1b1aff42c049
admin <andryuha_nikolaev@mail.ru>
2022-08-31 11:05:35 +0300

    fix: изменить сообщение авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new Unauthorized('Необходима авторизация');
+    throw new Unauthorized('Необходима авторизация!');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new Unauthorized('Необходима авторизация'));
+    next(new Unauthorized('Необходима авторизация!'));
     return;
   }
 
