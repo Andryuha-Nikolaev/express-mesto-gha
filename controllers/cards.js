ddd5b8936e006bac00a7be1cb2f5178c64030439
admin <andryuha_nikolaev@mail.ru>
2022-08-31 22:55:02 +0300

    fix: запрос получения карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,7 +19,7 @@ const createCard = (req, res, next) => {
 const getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => {
-      res.send({ cards });
+      res.send(cards);
     })
     .catch(next);
 };
