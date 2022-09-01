87dbcc303054849fe90d8a1b1d71f610ab27441d
admin <andryuha_nikolaev@mail.ru>
2022-09-01 15:14:11 +0300

    fix: финальная версия

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(cors);
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
